8454df0fac129daf2fbe85f830976ef4f6f065eb
altaf <altaf98khan66@gmail.com>
2023-04-29 16:06:15 +0530

    Add: To Total Number of Rides ,Total Fare and Average Fare Per Ride.

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,9 +1,17 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+
+	InvoiceGenerator invoiceGenerator = null;
+
+	@Before
+	public void setUp() throws Exception {
+		invoiceGenerator = new InvoiceGenerator();
+	}
 
 	@Test
 	public void givenDistanceAndTime() {
@@ -24,12 +32,13 @@ public class InvoiceGeneratorTest {
 	}
 
 	@Test
-	public void givenMultipleRides() {
+	public void givenMultipleRidesInvoiceSummery() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1)
 
 		};
-		double fare = invoiceGenerator.calculateFare(rides);
-		Assert.assertEquals(30, fare, 0.0);
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInvoiceSummary, summary);
 	}
 }
